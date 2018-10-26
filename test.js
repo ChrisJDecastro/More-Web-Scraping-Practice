fec2aa80770523b40a2c9f4acb479bbdf2983bbf
fuzionllf <fuzion117@gmail.com>
2018-10-26 00:31:01 -0700

    screenshot

---
+++ test.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+async function getPic(){
+    var num = Math.floor(Math.random()*100);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
+    console.log("Taking Screenshot");
+    await page.screenshot(
+        {
+           path: 'google' + num + '.png' 
+        });
+    var photoName = 'google' + num + '.png';
+
+    console.log("Took screenshot");
+    console.log("The name of the photo is " + photoName);
+
+    await browser.close();
+}
+
+getPic();
